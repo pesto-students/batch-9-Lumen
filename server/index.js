2fea74772e436afc50a09cf33e097ced9394bc56
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-13 08:49:02 +0530

    build(husky): setup husky hooks"
    fixes: #3

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
-import { config } from "dotenv";
-import dotenv from "dotenv";
-import express from "express";
+/* eslint-disable */
+import dotenv, { config } from 'dotenv';
+
+import express from 'express';
+
 const app = express();
 const port = 3001;
 
 dotenv.config();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
