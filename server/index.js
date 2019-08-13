cd084db8fbacd19ca76872cd23b179255e4bee0c
Sumit Nihalani <nihalanisumit@gmail.com>
2019-08-13 05:54:19 +0530

    add babel to the server side to use ES6 features
    fix #4

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
-const express = require("express");
+import { config } from "dotenv";
+import dotenv from "dotenv";
+import express from "express";
 const app = express();
 const port = 3001;
+
+dotenv.config();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
