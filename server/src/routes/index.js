b8288bb48d86e602ede0750afba6edace9d70f60
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-18 18:26:21 +0530

    fix(server): add routing for client

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,12 @@
 
+import path from 'path';
 import restRoutes from './rest';
 
 const init = (server) => {
   server.get('*', (req, res, next) => {
     console.log(`Request received for ${req.originalUrl}`);
-    return next();
+    res.sendFile(path.resolve(__dirname, '../../../client/src/index.jsx'));
+    next();
   });
 
   server.use('/api', restRoutes);
