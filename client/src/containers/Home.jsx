8962b5313b2380ee0781d864fe660580590857ae
Sumit Nihalani <nihalanisumit@gmail.com>
2019-08-19 17:41:43 +0530

    feat(client): design homepage and FAB
    Design home page component using flexbox, add dummy blog card component with placeholder and create floating action button to create a blog

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
+import styles from './Home.module.css';
+import Blog from '../components/Blog';
+import FloatingActionButton from '../components/FloatingActionButton/FloatingActionButton';
+
 
 const Home = () => (
-  <div>
-    <h1>HOME</h1>
-    <p>Home page with top 10 blogs</p>
-  </div>
+  <>
+    <h1> Top 10 trending blogs</h1>
+    <div className={styles.container}>
+      <div className={[styles.column, styles['column-first']].join(' ')}>
+        <div className={styles['large-blog']}>
+          <Blog />
+        </div>
+        <div className={styles['medium-blog']}>
+          <Blog />
+        </div>
+      </div>
+
+      <div className={[styles.column, styles['column-others']].join(' ')}>
+        <div className={styles['small-blog']}>
+          <Blog />
+        </div>
+        <div className={styles['small-blog']}>
+          <Blog />
+        </div>
+        <div className={styles['small-blog']}>
+          <Blog />
+        </div>
+        <div className={styles['small-blog']}>
+          <Blog />
+        </div>
+      </div>
+
+      <div className={[styles.column, styles['column-others']].join(' ')}>
+        <div className={styles['small-blog']}>
+          <Blog />
+        </div>
+        <div className={styles['small-blog']}>
+          <Blog />
+        </div>
+        <div className={styles['small-blog']}>
+          <Blog />
+        </div>
+        <div className={styles['small-blog']}>
+          <Blog />
+        </div>
+      </div>
+    </div>
+    <FloatingActionButton />
+  </>
 );
 
 export default Home;
