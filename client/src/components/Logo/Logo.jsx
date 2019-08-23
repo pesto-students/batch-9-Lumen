674826e36ba38e5fe5396601abbe337059d80a3f
Sumit Nihalani <nihalanisumit@gmail.com>
2019-08-24 04:01:34 +0530

    feat: add new logo

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import LumenLogo from '../../test.png';
+import LumenLogo from './LumenLogoWhite.png';
 import classes from './Logo.module.css';
 
 const logo = () => (
-  <NavLink to={"/"}>
+  <NavLink to="/">
     <div className={classes.Logo}>
       <img src={LumenLogo} alt="LumenLogo" />
     </div>
