0a15c5cef20e2b89c42e4db4151363422c9f1f80
Sumit Nihalani <nihalanisumit@gmail.com>
2019-09-01 02:59:14 +0530

    fix(client) improve css for category page

--- client/src/containers/Category/Category.jsx
+++ client/src/containers/Category/Category.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import MovableStackedCard from "../../components/MovableStackedCard/MovableStackedCard";
+import { Divider } from 'semantic-ui-react';
+import MovableStackedCard from '../../components/MovableStackedCard/MovableStackedCard';
 import styles from './Category.module.css';
 
-const Category = ({
-    categories = [],
-}) => {
-    const categoriesCards = categories.map((category) => {
-       return( <>
-       <h1 className={styles.title}> {category.name}</h1>
-            <MovableStackedCard {...category} />
-       </>)
-    })
+const Category = ({ categories = [] }) => {
+  const categoriesCards = categories.map(category => {
     return (
-        <div className={styles.background}>
-          {categoriesCards}
-        </div>
-    )
-}
-const mapStateToProps = (state) => ({
-    categories: state.home.categories,
-  })
-  
-  export default connect(mapStateToProps, {})(Category);
+      <>
+        <Divider inverted horizontal>
+          <h1 className={styles.title}> {category.name}</h1>
+        </Divider>
+        <MovableStackedCard {...category} />
+      </>
+    );
+  });
+  return (
+    <div className={styles.background}>
+      <div className={styles.container}>{categoriesCards}</div>
+    </div>
+  );
+};
+const mapStateToProps = state => ({
+  categories: state.home.categories
+});
+
+export default connect(
+  mapStateToProps,
+  {}
+)(Category);
