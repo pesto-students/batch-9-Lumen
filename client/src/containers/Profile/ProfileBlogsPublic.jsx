f153cba2951b16ca581f6aa7563e3f8fa1c8af48
Sumit Nihalani <nihalanisumit@gmail.com>
2019-08-31 17:53:53 +0530

    fix(react): change profile page css

--- client/src/containers/Profile/ProfileBlogsPublic.jsx
+++ client/src/containers/Profile/ProfileBlogsPublic.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Tab } from 'semantic-ui-react';
 import Blogcard from '../../components/BlogCard';
 import styles from './ProfileBlogs.module.css';
 import useGetUserBlogs from '../../hooks/useGetUserBlogs';
@@ -13,40 +12,11 @@ const ProfileBlogs = ({
     <Blogcard blog={blog} username={username} href={`/preview/${blog._id}`} />
     </div>
   ));
-  const panes =[
-    {
-      menuItem: 'Blogs',
-      render: () => (<Tab.Pane loading={!fetched}> {blogsCards} </Tab.Pane>),
-    },
-  ]
   
   return (
     <div>
-      {/* <Menu pointing secondary className={styles.menuStyle} inverted>
-        <Menu.Item
-          name="Published"
-          active={activeItem === 'blog'}
-          onClick={() => setActiveItem('blog')}
-        />
-        <Menu.Item
-          name="Drafts"
-          active={activeItem === 'draft'}
-          onClick={() => setActiveItem('draft')}
-        />
-      </Menu>
-      <div className={styles.blogcardContainer}>
-        <Blogcard />
-      </div>
-      <div className={styles.blogcardContainer}>
-        <Blogcard />
-      </div>
-      <div className={styles.blogcardContainer}>
-        <Blogcard />
-      </div>
-      <div className={styles.blogcardContainer}>
-        <Blogcard />
-      </div> */}
-      <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
+      <h1 className={styles.title}> Blogs </h1>
+      {blogsCards}
     </div>
   );
 };
