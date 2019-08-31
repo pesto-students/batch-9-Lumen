e15aa76582d2287cd7d9d07e1efe6d7b337dcfe2
Sumit Nihalani <nihalanisumit@gmail.com>
2019-09-01 04:55:29 +0530

    feat(client): design category type page

--- client/src/containers/CategoryType/CategoryType.jsx
+++ client/src/containers/CategoryType/CategoryType.jsx
@@ -1,42 +1,43 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable react/prop-types */
-import React from 'react'
+import React from 'react';
+import { Divider } from 'semantic-ui-react';
 import useSearchBlogs from '../../hooks/useSearchBlogs';
 import BlogCard from '../../components/BlogCard';
 import Loader from '../../components/UI/Loader';
+import classes from './CategoryType.module.css';
 
-const CategoryType = ({match,
-     location:{
-          search },
-     history}) => {
-    const queryParams = new URLSearchParams(search);
-    const requiredSearchParam = queryParams.get('search');
-    if(!requiredSearchParam) {
-        history.push('/')
-    }
-    const categoryList = [requiredSearchParam];
-    const [blogs, fetched] = useSearchBlogs(1, '', categoryList);
-    const blogsCards = blogs.map((blog) => {
-        const user = blog.userId || {};
-        return ( <BlogCard {...user} {...blog} href={`/blog/${blog._id}`}/>)
-    }
-    )
-
-    if(!fetched) {
-        return(
-            <Loader 
-            size="massive"
-            text="Loading.."
-            />
-        )
-    }
+const CategoryType = ({ match, location: { search }, history }) => {
+  const queryParams = new URLSearchParams(search);
+  const requiredSearchParam = queryParams.get('search');
+  if (!requiredSearchParam) {
+    history.push('/');
+  }
+  const categoryList = [requiredSearchParam];
+  const [blogs, fetched] = useSearchBlogs(1, '', categoryList);
+  const blogsCards = blogs.map(blog => {
+    const user = blog.userId || {};
     return (
-        <div>
-            <h1>{match.params.type}</h1>
-            {blogsCards}
-        </div>
-    )
-}
+      <BlogCard {...user} {...blog} href={`/blog/${blog._id}`} key={blog._id} />
+    );
+  });
 
-export default CategoryType
+  if (!fetched) {
+    return <Loader size="massive" text="Loading.." />;
+  }
+  return (
+    <div className={classes.background}>
+      <div className={classes.container}>
+        <div style={{ paddingBottom: '20px' }}>
+          <Divider inverted horizontal>
+            <h1 className={classes.title}>{match.params.type}</h1>
+          </Divider>
+        </div>
+        {blogsCards}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryType;
