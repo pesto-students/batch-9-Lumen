be6e6894dab98705a4ad0e73601efb7e44cc6ead
piyushagwl007 <piyushagwl007@gmail.com>
2019-09-01 00:58:07 +0530

    feat(client): add updated lumen logo

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import LumenLogo from './LumenLogoWhite.png';
 import classes from './Logo.module.css';
+
+const smallestLogo =
+  'https://res.cloudinary.com/dw1omslee/image/upload/c_scale,w_50/v1567267475/Lumen/Lumen-logo-mobile_lgjgq7.png';
+// const slightLargeLogo = 'https://res.cloudinary.com/dw1omslee/image/upload/c_scale,w_100/v1567267475/Lumen/Lumen-logo-mobile_lgjgq7.png';
+const mediumLogo =
+  'https://res.cloudinary.com/dw1omslee/image/upload/c_scale,w_100/v1567267732/Lumen/logo-full_hozy1u.png';
+// const largeLogo = 'https://res.cloudinary.com/dw1omslee/image/upload/c_scale,w_200/v1567267732/Lumen/logo-full_hozy1u.png';
+// const hugeLogo = 'https://res.cloudinary.com/dw1omslee/image/upload/c_scale,w_300/v1567267732/Lumen/logo-full_hozy1u.png';
 
 const logo = () => (
   <NavLink to="/">
     <div className={classes.Logo}>
-      <img src={LumenLogo} alt="LumenLogo" />
+      <img
+        src={smallestLogo}
+        className={classes.image_mobile}
+        alt="Lumen Logo"
+      />
+      <img src={mediumLogo} className={classes.image_full} alt="Lumen Logo" />
     </div>
   </NavLink>
 );
